09ca2354c37f7353bf411cb6da50e68cc9299a03
Thành Nguyễn <nguyennhuthanh0104@gmail.com>
2026-01-15 00:54:07 +0700

    add package.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,40 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors";
+import path from "node:path";
 
 import router from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT ?? 3000;
+const __dirname = path.resolve();
 
 // middleware
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+      cors({
+          origin: "http://localhost:5173",
+      })
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
-app.use("/api/notes", router);
+app.use("/api/notes", router);   // api
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));  // Static html
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
